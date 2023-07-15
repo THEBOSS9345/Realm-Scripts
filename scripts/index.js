cd8f815d1cba0b45dcaeae7ee9d0d4fdb5688163
THE_BOSS9345 <125090249+THEBOSS5439@users.noreply.github.com>
2023-07-15 16:19:21 -0400

    add Advanced-Floating-Items to the manifest

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 import './ChatHandler/index';
 import './Entity_Heath_Show/index';
 import './Sell_Pad/index'
-import './Spawn-protection/index.js;
+import './Spawn-protection/index.js';
+import './Advanced-Floating-Items/index.js'
