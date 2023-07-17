e5120b1e27fb9669e6ab1789f2fdbebf0ac8ad11
THE_BOSS9345 <125090249+THEBOSS5439@users.noreply.github.com>
2023-07-17 17:14:14 -0400

    Update The code

--- scripts/Entity_Heath_Show/index.js
+++ scripts/Entity_Heath_Show/index.js
@@ -3,10 +3,11 @@ const Entity_Health_Database = new Map();
 system.runInterval(() => {
   world.getDimension('overworld').getEntities().forEach(entity => {
     const typeId = entity.typeId;
-    const health =  Math.ceil(entity.getComponent('health')?.defaultValue);
+    if (typeId === 'minecraft:player') return
+    const health =  Math.ceil(entity.getComponent('health')?.defaultValue).toFixed(2) / 2;
     if (!Entity_Health_Database.has(typeId)) {
       Entity_Health_Database.set(typeId, health);
     }
-    entity.nameTag = `${typeId.replace('minecraft:', '').toLocaleUpperCase()}\n${Math.ceil(entity.getComponent('health')?.currentValue)}/${Entity_Health_Database.get(typeId)}`;
+    entity.nameTag = `${typeId.replace('minecraft:', '').toLocaleUpperCase()}\n${Math.ceil(entity.getComponent('health')?.currentValue) / 2} / ${Entity_Health_Database.get(typeId)}`;
   });
 });
