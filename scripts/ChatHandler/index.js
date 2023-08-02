166d83f8b55e1416669b585d97229af4c9d17fcc
THE_BOSS9345 <125090249+THEBOSS5439@users.noreply.github.com>
2023-08-02 11:28:47 -0400

    Update code

--- scripts/ChatHandler/index.js
+++ scripts/ChatHandler/index.js
@@ -3,15 +3,18 @@ import { commandDatabase } from './Chat_Commands';
 
 world.beforeEvents.chatSend.subscribe((data) => {
   const { message, sender: player } = data;
-  if (!message.startsWith('.')) return;
+  if (!message.startsWith(commandPrefix)) return;
   data.cancel = true;
-  const args = message.slice(1).trim().split(' ');
-  const commandName = args.shift();
-  const command = commandDatabase.find(({ command, alias }) => command === commandName || (alias && alias.includes(commandName)));
-  const hasPermission = (tags) => tags.some(permission => player.hasTag(permission));
-  if (command && (!command.permissions || hasPermission(command.permissions))) {
-    command.callback(player, args.join(' '));
+  const commandString = message.slice(commandPrefix.length).trim();
+    const hasPermission = (tags) => tags.some(permission => player.hasTag(permission));
+  const matchedCommand = commandDatabase.find(({ command, alias }) => {
+    const cmdRegex = new RegExp(`^${command}(\\s|$)`, 'i');
+    return cmdRegex.test(commandString) || (alias && alias.some(a => new RegExp(`^${a}(\\s|$)`, 'i').test(commandString)));
+  });
+  if (matchedCommand && (!matchedCommand.permissions || hasPermission(matchedCommand.permissions))) {
+    const args = commandString.slice(matchedCommand.command.length).trim();
+    matchedCommand.callback(player, args);
     return;
   }
-  player.sendMessage('Unknown command or insufficient permissions.');
+  player.sendMessage(`§cUnknown command: ${commandString}, Please check that the command exists and that you have permission to use it.`);
 });
