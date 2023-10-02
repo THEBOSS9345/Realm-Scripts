8287967d521d55b2b08e5aeb174cd65850d90db6
THE_BOSS9345 <125090249+THEBOSS9345@users.noreply.github.com>
2023-10-02 01:01:47 -0700

    Update index.js

--- scripts/Advanced-Floating-Items/index.js
+++ scripts/Advanced-Floating-Items/index.js
@@ -1,6 +1,5 @@
 import { world } from '@minecraft/server';
-world.afterEvents.entitySpawn.subscribe((data) => {
-    const { entity } = data
+world.afterEvents.entitySpawn.subscribe(({ entity }) => {
     if (entity.typeId != "minecraft:item") return;
     const ItemStack = entity.getComponent("item").itemStack;
     entity.nameTag = `§6${ItemStack.amount}x§r ${ItemStack.typeId.replace('minecraft:', '').toUpperCase()}`;
