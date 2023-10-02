a6df7990eaa138782079ea54852bf83091f054d0
THE_BOSS9345 <125090249+THEBOSS9345@users.noreply.github.com>
2023-10-02 00:54:18 -0700

    Delete scripts/index.js

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-import './ChatHandler/index';
-import './Entity_Heath_Show/index';
-import './Sell_Pad/index'
-import './Spawn-protection/index.js';
-import './Advanced-Floating-Items/index.js'
-import './Anti-Log/index.js'
