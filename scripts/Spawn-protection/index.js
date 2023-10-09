d98534d65783a21047c78c1aaf7eee2233fe02eb
THE_BOSS9345 <125090249+THEBOSS9345@users.noreply.github.com>
2023-10-09 11:36:02 -0700

    Update index.js

--- scripts/Spawn-protection/index.js
+++ scripts/Spawn-protection/index.js
@@ -1,50 +1,26 @@
-import { world, system, Vector } from '@minecraft/server';
+import { BlockVolumeUtils, BoundingBoxUtils, Vector, world, system } from '@minecraft/server';
 
-const spawn = new SpawnPro();
-spawn.setSpawn(1, -60, 1, 99);
 class SpawnPro {
-    setSpawn(x, y, z, radius) {
-        this.x = x || 0;
-        this.y = y || 0;
-        this.z = z || 0;
-        this.radius = radius || 0;
+    constructor() {
+        this.box = (players, res) => players.some(player => BoundingBoxUtils.isInside(BoundingBoxUtils.createValid(res.data.from, res.data.to), player.location));
+        this.spawnLocations = [];
+        this.playersInside = new Set();
+        system.runInterval(() => this.updatePlayerLocations());
+        world.beforeEvents.itemUseOn.subscribe((data) => this.playersInside.has(data.source.name) ? (data.cancel = true) : null);
+        world.beforeEvents.playerBreakBlock.subscribe((data) => this.playersInside.has(data.player.name) ? (data.cancel = true) : null);
+    }
+    setSpawn(locations) {
+        this.spawnLocations = locations.map((location) => ({ from: new Vector(location.from.x, location.from.y, location.from.z), to: new Vector(location.to.x, location.to.y, location.to.z) }));
+    }
+    updatePlayerLocations() {
+        for (const player of world.getPlayers()) {
+            const isInsideAnyBox = this.spawnLocations.some((location) => this.box([player], { data: { from: location.from, to: location.to } }));
+            if (isInsideAnyBox) this.playersInside.add(player.name); else this.playersInside.delete(player.name);
+        }
     }
 }
 
-world.beforeEvents.itemUseOn.subscribe((data) => {
-    if (radiusCheck(data.source, spawn?.x, spawn?.y, spawn?.z, spawn?.radius)) {
-       data.cancel = true;
-    }
-});
-
-world.afterEvents.blockBreak.subscribe(async ({ player, block, brokenBlockPermutation }) => {
-    if (radiusCheck(player, spawn?.x, spawn?.y, spawn?.z, spawn?.radius)) {
-        block.setPermutation(brokenBlockPermutation);
-        await wait(1);
-        const items = player.dimension.getEntities({
-            type: 'minecraft:item',
-            location: new Vector(block.location.x, block.location.y, block.location.z),
-        });
-        await kill(items);
-    }
-});
-
-
-async function kill(items) {
-    for (const item of items) item.kill();
-}
-
-async function wait(ticks) {
-    return new Promise((resolve) => {
-        system.runTimeout(resolve, ticks);
-    });
-}
-function radiusCheck(player, x, y, z, radius) {
-    const playerLocation = player.location;
-    const playerX = playerLocation.x;
-    const playerY = playerLocation.y;
-    const playerZ = playerLocation.z;
-    const distanceSquared = (playerX - x) ** 2 + (playerY - y) ** 2 + (playerZ - z) ** 2;
-    const radiusSquared = radius ** 2;
-    return distanceSquared <= radiusSquared;
-}
+const spawnProInstance = new SpawnPro();
+spawnProInstance.setSpawn([
+    { from: { x: -116, y: -41, z: 186 }, to: { x: 123, y: 207, z: -73 } },
+]); 
