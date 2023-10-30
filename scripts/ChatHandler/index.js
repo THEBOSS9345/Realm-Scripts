99d5aa728f6a23a735af0e6fcf8ea4c5d9bd7107
THE_BOSS9345 <125090249+THEBOSS9345@users.noreply.github.com>
2023-10-29 19:36:19 -0700

    Update index.js

--- scripts/ChatHandler/index.js
+++ scripts/ChatHandler/index.js
@@ -1,80 +1,47 @@
-import { Player, World, world } from '@minecraft/server';
-
-export class ChatCommand {
-  static instance = null;
-  static commandDatabase = [];
-
-  constructor() {
-    if (!ChatCommand.instance) {
-      console.warn(JSON.stringify(ChatCommand.commandDatabase));
-      this.commandPrefix = '.';
-      world.beforeEvents.chatSend.subscribe(this.handleChatCommand.bind(this));
-      ChatCommand.instance = this;
+import { world } from '@minecraft/server';
+const commands = []
+let CommandInitialized = false;
+Object.defineProperty(globalThis, 'ChatCommand', {
+    get: function () {
+        const prefix = '.';
+        const hasPermission = (player, tags) => tags.some(permission => player.hasTag(permission));
+        return {
+            create(command, description, alias = [], permissions = [], callback) {
+                if (typeof description !== 'string' || typeof command !== 'string') return console.error(`${typeof command === "string" ? 'description' : 'name'} is not a string at create`);
+                commands.push({ command, description, alias, permissions, callback });
+                ChatCommand.handleChatCommand
+            },
+            get handleChatCommand() {
+                if (CommandInitialized) return;
+                CommandInitialized = true;
+                world.beforeEvents.chatSend.subscribe((data) => {
+                    const { message, sender: player } = data;
+                    if (!message.startsWith(prefix)) return;
+                    data.cancel = true;
+                    const commandString = message.slice(prefix.length).trim();
+                    const matchedCommand = commands.find(({ command, alias }) => {
+                        const cmdRegex = new RegExp(`^${command}(\\s|$)`, 'i');
+                        return cmdRegex.test(commandString) || (alias && alias.some(a => new RegExp(`^${a}(\\s|$)`, 'i').test(commandString)));
+                    });
+                    if (matchedCommand && (matchedCommand.permissions.length === 0 || hasPermission(matchedCommand.permissions, player))) {
+                        const args = commandString.slice(matchedCommand.command.length).trim();
+                        matchedCommand.callback(player, args, commandString);
+                    } else player.sendMessage(`§cUnknown command: ${commandString}, Please check that the command exists and that you have permission to use it.`);
+                });
+            }
+        };
     }
-
-    return ChatCommand.instance;
-  }
-
-  /**
-   * Create a new chat command.
-   * 
-   * @param {string} command - The command string (e.g., "help").
-   * @param {string} description - Description of the command.
-   * @param {string[]} alias - An array of alias strings for the command.
-   * @param {string[]} permissions - An array of required permissions.
-   * @param {(player: Player, args: string, commandString: string) => void} callback - The callback function to execute when the command is used.
-   */
-  create(command, description, alias, permissions, callback) {
-    ChatCommand.commandDatabase.push({ command, description, alias, permissions, callback });
-  }
-
-  /**
-   * Handle incoming chat commands.
-   * 
-   * @param {object} data - The chat data object.
-   * @param {string} data.message - The chat message.
-   * @param {Player} data.sender - The player who sent the message.
-   */
-  handleChatCommand(data) {
-    const { message, sender: player } = data;
-    if (!message.startsWith(this.commandPrefix)) return;
-    data.cancel = true;
-    const commandString = message.slice(this.commandPrefix.length).trim();
-    const matchedCommand = ChatCommand.commandDatabase.find(({ command, alias }) => {
-      const cmdRegex = new RegExp(`^${command}(\\s|$)`, 'i');
-      return cmdRegex.test(commandString) || (alias && alias.some(a => new RegExp(`^${a}(\\s|$)`, 'i').test(commandString)));
-    });
-    if (matchedCommand && (matchedCommand.permissions.length === 0 || this.hasPermission(matchedCommand.permissions, player))) {
-      const args = commandString.slice(matchedCommand.command.length).trim();
-      matchedCommand.callback(player, args, commandString);
-    } else player.sendMessage(`§cUnknown command: ${commandString}, Please check that the command exists and that you have permission to use it.`);
-  }
-
-  /**
-   * Check if a player has at least one of the required permissions.
-   * 
-   * @param {string[]} tags - An array of permission tags.
-   * @param {Player} player - The player to check.
-   * @returns {boolean} - True if the player has at least one required permission, otherwise false.
-   */
-  hasPermission(tags, player) {
-    return tags.some(permission => player.hasTag(permission));
-  }
-}
-ChatCommand.commandDatabase = [];
-
-const newCommand = new ChatCommand();
-
-newCommand.create('ping', '', [], [], (player) => {
-    player.sendMessage('Server Ping')
 });
-newCommand.create('Help', 'Help Command What Show All Commands', ['h', 'help'], [], (player) => {
-  const helpMessage = newCommand.commandDatabase
-    .filter(command => command.permissions.length === 0 || newCommand.hasPermission(command.permissions, player))
-    .map(command => {
-      const alias = command.alias ? `[${command.alias.join(', ')}] ` : '';
-      const description = command.description ? command.description : 'No description';
-      return `§7${command.command} - ${alias}${description}`;
-    }).join('\n');
-  player.sendMessage(`§f§aAvailable Commands\n§f-------------------------\n${helpMessage}\n`);
+ChatCommand.create('Help', 'Help Command: Shows all available commands', ['h', 'help'], [], (player) => {
+    const helpMessage = commands
+        .filter(command => command.permissions.length === 0 || hasPermission(player, command.permissions))
+        .map(command => {
+            const alias = command.alias.length > 0 ? `[${command.alias.join(', ')}] ` : '';
+            const description = command.description ? command.description : '';
+            return `§7${command.command} - ${alias}${description}`;
+        })
+        .join('\n');
+    player.sendMessage(`§f§aAvailable Commands\n§f-------------------------\n${helpMessage}\n`);
 });
+
+export default globalThis.ChatCommand
