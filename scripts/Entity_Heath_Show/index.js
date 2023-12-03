21db445e26b4c9ac9447dac86653b83511378aad
THE_BOSS9345 <125090249+THEBOSS9345@users.noreply.github.com>
2023-12-02 20:08:02 -0800

    Delete scripts directory

--- scripts/Entity_Heath_Show/index.js
+++
@@ -1,9 +0,0 @@
-import { world, system, EntityHealthComponent } from "@minecraft/server";
-system.runInterval(() => {
-  world.getDimension('overworld').getEntities().forEach(entity => {
-    const typeId = entity.typeId;
-    if (typeId === 'minecraft:player') return
-    const maxhealth =  Math.ceil(entity.getComponent('health')?.defaultValue).toFixed(2) / 2;
-    entity.nameTag = `${typeId.replace('minecraft:', '').toLocaleUpperCase()}\n${Math.ceil(entity.getComponent('health')?.currentValue) / 2} / ${maxhealth}`;
-  });
-});
