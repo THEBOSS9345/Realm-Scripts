4e7b95e8c13b90c76531099d524443e19f6a61fc
THE_BOSS9345 <125090249+THEBOSS9345@users.noreply.github.com>
2024-01-08 23:15:01 -0800

    Update index.js

--- Anti-log/index.js
+++ Anti-log/index.js
@@ -1,38 +1,68 @@
-import { world, system, Player } from '@minecraft/server';
+import { world, system } from "@minecraft/server";
+export const CombatDatabase = {};
 
-const events = {
-  database: new Map(),
-  timer: new Map(),
-  timerValue: 10,
-  callback() {
-    this.EntityHit = world.afterEvents.entityHitEntity.subscribe(({ damagingEntity: entity, hitEntity }) => {
-      if (!(hitEntity instanceof Player) && !(entity instanceof Player)) return
-      this.timer.set(entity.id, Date.now());
-    });
-    this.Run = system.runInterval(() => {
-      for (const player of world.getPlayers()) {
-        if (!this.timer.has(player.id)) return;
-        this.database.set(player.id, [player.dimension.id, player.location, {Items: [["Head", "Chest", "Legs", "Feet"].map(v => player.getComponent("minecraft:equippable").getEquipment(v)).filter(v => v), Array.from({ length: 36 }).map((_, i) => player.getComponent('inventory').container.getItem(i)).filter(v => v !== undefined)].flat()}]);
-      }
-    });
-    this.Leave = world.afterEvents.playerLeave.subscribe(({ playerId, playerName }) => {
-      if (!this.timer.has(playerId)) return;
-      if (Date.now() - this.timerValue * 1000 > this.timer.get(playerId)) return;
-      const info = this.database.get(playerId);
-      for (const itemsInfo of info[2].Items) {
-          console.warn(JSON.stringify(info[1]), itemsInfo.typeId)
-      world.getDimension(info[0]).spawnItem(itemsInfo, info[1]);
+world.afterEvents.entityHurt.subscribe((event) => {
+    if (event.damageSource.cause !== "entityAttack") return;
+    CombatDatabase[event.hurtEntity.id] = { timer: setTimer(20, 'seconds') };
+    event.hurtEntity.addTag('incombat');
+}, { entityTypes: ["minecraft:player"] })
+
+system.runInterval(() => {
+    world.getPlayers({ tag: 'incombat' }).map((player) => {
+        if (!CombatDatabase[player.id]) return delete CombatDatabase[player.id], player.removeTag('incombat')
+        if (!CombatDatabase[player.id] || CombatDatabase[player.id].hasOwnProperty('clear')) return;
+        if (hasTimerReachedEnd(CombatDatabase[player.id].timer.targetDate)) {
+            delete CombatDatabase[player.id]
+            player.sendMessage('§aYou Are Now Out Of Combat')
+            player.removeTag('incombat')
+            return
+        }
+        const playerinv = player.getComponent('inventory').container
+        CombatDatabase[player.id] = { timer: CombatDatabase[player.id].timer, location: player.location, dimension: player.dimension.id, items: [...Array.from({ length: playerinv.size }).map((_, i) => playerinv.getItem(i)).filter(v => v !== undefined), ...["Head", "Chest", "Legs", "Feet"].map(v => player.getComponent("minecraft:equippable").getEquipment(v)).filter(v => v !== undefined)] }
+    })
+})
+
+world.afterEvents.playerLeave.subscribe(({ playerId, playerName }) => {
+    if (!CombatDatabase[playerId] || CombatDatabase[playerId]?.clear) return;
+    CombatDatabase[playerId]?.items.map((value) => world.getDimension(CombatDatabase[playerId].dimension).spawnItem(value, CombatDatabase[playerId].location))
+    CombatDatabase[playerId] = { clear: true }
+    console.warn(`[Combat Logging] ${playerName} Combat Logged!`)
+})
+
+world.afterEvents.playerSpawn.subscribe((event) => {
+    if (!event.initialSpawn) return
+    if (!CombatDatabase[event.player.id]?.clear) return;
+    delete CombatDatabase[event.player.id]
+    event.player.runCommandAsync('clear @s')
+    event.player.sendMessage('§cYour inventory Was Cleared For Combat logging!');
+})
+
+world.afterEvents.entityDie.subscribe(({ damageSource, deadEntity }) => {
+    if (!CombatDatabase[deadEntity.id]) return;
+    delete CombatDatabase[deadEntity.id]
+    deadEntity.sendMessage('§aCombat Ended')
+}, { entityTypes: ["minecraft:player"] })
+
+export const setTimer = (value, unit) => {
+    const targetDate = new Date();
+    switch (unit) {
+        case 'hours':
+            targetDate.setHours(targetDate.getHours() + value);
+            break;
+        case 'days':
+            targetDate.setDate(targetDate.getDate() + value);
+            break;
+        case 'minutes':
+            targetDate.setMinutes(targetDate.getMinutes() + value);
+            break;
+        case 'seconds':
+            targetDate.setSeconds(targetDate.getSeconds() + value);
+            break;
     }
-  });
-    this.Spawn = world.afterEvents.playerSpawn.subscribe(async ({ player, initialSpawn }) => {
-      if (!initialSpawn) return;
-      if (!(Date.now() - this.timerValue * 1000 > this.timer.get(player.id))) return;
-      player.runCommandAsync('clear @s')
-      world.sendMessage('The Inv Is Been Cleared')
-      this.timer.delete(player.id);
-      this.database.delete(player.id);
-    });
-  }
+    return { value, unit, targetDate };
 };
 
-events.callback();
+export function hasTimerReachedEnd(targetDate) {
+    if (!(targetDate instanceof Date)) targetDate = new Date(targetDate);
+    return Date.now() >= targetDate;
+}
